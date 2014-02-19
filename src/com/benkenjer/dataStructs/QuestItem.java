f7051ab19b2e3107ccfcf8d549c9cbcbc9548344
Beknight <benjamin.fan11.11@gmail.com>
2014-02-20 09:35:19 +1100

    added the new items to git

---
+++ src/com/benkenjer/dataStructs/QuestItem.java
@@ -0,0 +1,36 @@
+package com.benkenjer.dataStructs;
+
+import java.util.LinkedList;
+
+public class QuestItem {
+
+	private String name;
+	private long createTime;
+	private long deadLine;
+	private LinkedList<String> objectiveList;
+
+	public QuestItem(String name, long createTime, long deadLine,
+			LinkedList<String> objectiveList) {
+		this.name = name;
+		this.createTime = createTime;
+		this.deadLine = deadLine;
+		this.objectiveList = objectiveList;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public long getStartTime(){
+		return createTime;
+	}
+	
+	public long getDeadLine(){
+		return deadLine;
+	}
+	
+	public LinkedList<String> getObjectiveList(){
+		return objectiveList;
+	}
+	
+}
